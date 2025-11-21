99ad10a465f4da155681bbe70a361e4c86ada5a1
miazi2003 <yeasinmiazi1997@gmail.com>
2025-11-21 18:56:31 +0600

    new added again=

--- models/Order.js
+++ models/Order.js
@@ -1,10 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-  customerID: { type: Number, required: true },
-  futureOrderDate: { type: String },
-  productName: { type: String },
-  // add other order fields as needed
-}, { timestamps: true });
+const orderSchema = new mongoose.Schema(
+  {
+    customerID: { type: Number, required: true },
+    customerName: { type: String },
+    mobile: { type: String },
+    address: { type: String },
+    joinDate: { type: String },
 
-module.exports = mongoose.model('orders', orderSchema);
+    orderDate: { type: String, required: true },
+    futureOrderDate: { type: String },
+
+    products: [
+      {
+        productId: { type: Number, required: true },
+        productName: { type: String, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("orders", orderSchema);
